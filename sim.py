d2f495f2683d491f91f1d7612ee26b9b293b758d
Stamk <stkostas1995@gmail.com>
2020-05-25 02:44:23 +0300

    compatible sim file

--- sim.py
+++ sim.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     ag1 = Agent()
     ag2 = Agent()
     print("--------------------------------- Q-learning ---------------------------------")
-    ag1.Q_values(flag=1)  # flag==1 for Q-learning
+    ag1.Q_values(str="Q-learning")
     print("----------------------------------- SARSA ------------------------------------")
-    ag2.Q_values(flag=0)  # else SARSA
+    ag2.Q_values(str="SARSA")
