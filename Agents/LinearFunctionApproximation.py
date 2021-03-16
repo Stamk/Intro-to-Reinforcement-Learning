625e6cd426a674fabd172e6660f65bc93b380e48
Stamk <stkostas1995@gmail.com>
2021-03-16 18:39:34 +0200

    LFA with qlearning update

--- Agents/LinearFunctionApproximation.py
+++ Agents/LinearFunctionApproximation.py
@@ -8,14 +8,11 @@ class Linear(Agent):
         def __init__(self, env, num_episodes, gamma, epsilon, alpha):
             super(Linear, self).__init__(env, num_episodes, gamma)
             #initialize weight and bias
-            self.w_shape = tuple(self.env.observation_space.nvec) + (self.env.action_space.n,)
-            self.w = np.ones([4])
-            self.x=np.ones([4,1])
-            self.bias = 0.0
+            self.w = np.zeros([9])
+            self.bias = 1
             self.epsilon = epsilon
             self.gamma = gamma
             self.alpha = alpha
-            self.h=1
 
         def choose_action(self, state):
             if random.uniform(0, 1) < self.epsilon:
@@ -25,35 +22,33 @@ class Linear(Agent):
             return action
         #update parameters
         def choose_best_action(self, state):
-            return self.env.action_space.sample()
+            if (self.linear_calc(state, 0) > self.linear_calc(state, 1)): return 0
+            else: return 1
 
         def update(self, state, action, new_state, reward, done, current_episode, episode_length):
-            lqf_s_a = self.linear_calc(state, action) #actions given state s
-            new_action = self.choose_action(new_state)
-            lqf_s_a_next = self.linear_calc(new_state, new_action)
-            delta=reward+self.gamma*lqf_s_a_next-lqf_s_a
-            print(lqf_s_a,lqf_s_a_next)
-            for i in range(0,4):
-             self.w[i]=self.w[i]+self.h*delta*state[i]
-#            TD_error = self.alpha * (lqf_s_a - (reward + self.gamma * lqf_s_a_next))
-#            if TD_error != 0.0:
-#                for state,v in states: #i = row, a = column, v = value of the state
-#                    self.w[state, action] = self.w[state, action] - TD_error * v
-#                self.bias = self.bias - TD_error
+            prediction = self.linear_calc(state, action) #actions given state s
+            new_action = self.choose_best_action(new_state)
+            target = self.linear_calc(new_state, new_action)
+            delta=reward+self.gamma*target-prediction
+            feature=self.get_feature_from_state_action(state,action)
+            TD_error = self.alpha * delta
+            for i in range(len(self.w)):
+             self.w[i]=self.w[i]+TD_error*feature[i]
+            self.bias = self.bias + TD_error
             return (False or done)
 
         #return q(s, a; w) for a given state s and action a
         def linear_calc(self, state, action):
             product = 0.0
-#            for state,v in states:
-#                product += v * self.w[state, action]
-            product=np.dot(self.w,state)
-            return product + self.bias
+            feature=self.get_feature_from_state_action(state,action)
+            product=np.dot(self.w,feature)
+            return product
 
-
-'''        def update_weights(self, weights, g):
-            for i in range(len(weights)):
-                for param in weights[i].keys():
-                    weights[i][param] += self.step_size * g[i][param]
-'''
-
+        def get_feature_from_state_action(self, state, action):
+            #feature vector
+            feature = np.zeros(len(self.w))
+            offset = action * len(state)
+            for i in range(len(state)):
+                feature[i + offset] = state[i]
+            feature[-1]=self.bias
+            return feature
