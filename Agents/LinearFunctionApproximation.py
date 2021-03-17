46d8473473ea7881efd6f53a8593da2da34517cc
Stamk <stkostas1995@gmail.com>
2021-03-17 20:44:33 +0200

    Update linear Qlearing

--- Agents/LinearFunctionApproximation.py
+++ Agents/LinearFunctionApproximation.py
@@ -8,7 +8,7 @@ class Linear(Agent):
         def __init__(self, env, num_episodes, gamma, epsilon, alpha):
             super(Linear, self).__init__(env, num_episodes, gamma)
             #initialize weight and bias
-            self.w = np.zeros([9])
+            self.w = np.zeros([10])
             self.bias = 1
             self.epsilon = epsilon
             self.gamma = gamma
@@ -30,16 +30,29 @@ class Linear(Agent):
             new_action = self.choose_best_action(new_state)
             target = self.linear_calc(new_state, new_action)
             delta=reward+self.gamma*target-prediction
+            print(target,prediction)
+            print(delta)
+            state=np.concatenate((state,np.array([1])),axis=0)
             feature=self.get_feature_from_state_action(state,action)
             TD_error = self.alpha * delta
             self.w=self.w+TD_error*feature
-            self.bias = self.bias + TD_error
+         #   self.bias = self.bias + TD_error
+            #                    self.w[state, action] = self.w[state, action] - TD_error * v
+            #                self.bias = self.bias - TD_error
+#            TD_error = self.alpha * (lqf_s_a - (reward + self.gamma * lqf_s_a_next))
+#            if TD_error != 0.0:
+#                for state,v in states: #i = row, a = column, v = value of the state
+#                    self.w[state, action] = self.w[state, action] - TD_error * v
+#                self.bias = self.bias - TD_error
             return (False or done)
 
         #return q(s, a; w) for a given state s and action a
         def linear_calc(self, state, action):
             product = 0.0
+            state=np.concatenate((state,np.array([1])),axis=0)
             feature=self.get_feature_from_state_action(state,action)
+#            for state,v in states:
+#                product += v * self.w[state, action]
             product=np.dot(self.w,feature)
             return product
 
@@ -49,5 +62,12 @@ class Linear(Agent):
             offset = action * len(state)
             for i in range(len(state)):
                 feature[i + offset] = state[i]
-            feature[-1]=self.bias
+            #feature[-1]=self.bias
             return feature
+
+'''        def update_weights(self, weights, g):
+            for i in range(len(weights)):
+                for param in weights[i].keys():
+                    weights[i][param] += self.step_size * g[i][param]
+'''
+
