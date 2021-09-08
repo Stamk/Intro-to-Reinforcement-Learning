351fe01b32c97013bea23b92b89f785f3fb913d4
Konstantinos <stkostas1995@gmail.com>
2021-09-09 00:25:55 +0300

    discrete action space with only one dimension

--- functions/wrappers.py
+++ functions/wrappers.py
@@ -5,18 +5,18 @@ import gym
 class StateDiscretize(gym.ObservationWrapper):
     def __init__(self, env, stepsizes=[],low=None, high=None):
         super(StateDiscretize, self).__init__(env)
-        self.stepsizes = stepsizes
+        self.Statestepsizes = stepsizes
 
         if high is not None:
             self.env.observation_space.high = np.array(high)
         if low is not None:
             self.env.observation_space.low = np.array(-low)
 
-        self.Statebins = np.empty(len(self.stepsizes), dtype=object)
-        for i in range(len(self.stepsizes)):
-            self.Statebins[i] = np.linspace(self.env.observation_space.low[i], self.env.observation_space.high[i],self.stepsizes[i] - 1)
+        self.Statebins = np.empty(len(self.Statestepsizes), dtype=object)
+        for i in range(len(self.Statestepsizes)):
+            self.Statebins[i] = np.linspace(self.env.observation_space.low[i], self.env.observation_space.high[i],self.Statestepsizes[i] - 1)
         # self.env.observation_space = gym.spaces.Box(self.env.observation_space.low, self.env.observation_space.high,shape=(self.env.observation_space.shape[0], stepsize), dtype=np.float32)
-        self.observation_space = gym.spaces.MultiDiscrete(self.stepsizes)
+        self.observation_space = gym.spaces.MultiDiscrete(self.Statestepsizes)
         print(self.Statebins)
 
     def observation(self, observation):
@@ -28,21 +28,20 @@ class StateDiscretize(gym.ObservationWrapper):
 class ActionDiscretize(gym.ActionWrapper):
     def __init__(self, env, stepsizes=[], low=None, high=None):
         super(ActionDiscretize, self).__init__(env)
-        self.stepsizes = stepsizes
+        self.Actionstepsizes = stepsizes
 
         if high is not None:
             self.env.action_space.high = np.array(high)
         if low is not None:
             self.env.action_space.low = np.array(low)
 
-        self.Actionbins = np.empty(len(self.stepsizes), dtype=object)
-        for i in range(len(self.stepsizes)):
-            self.Actionbins[i] = np.linspace(self.env.action_space.low[i], self.env.action_space.high[i],self.stepsizes[i] - 1)        # self.env.observation_space = gym.spaces.Box(self.env.observation_space.low, self.env.observation_space.high,shape=(self.env.observation_space.shape[0], stepsize), dtype=np.float32)
-        self.action_space = gym.spaces.MultiDiscrete(self.stepsizes)
+        self.Actionbins = np.empty(len(self.Actionstepsizes), dtype=object)
+        #TODO fix Actionbins for more dimensions
+        self.Actionbins = np.linspace(self.env.action_space.low[0], self.env.action_space.high[0],self.Actionstepsizes[0] - 1)        # self.env.observation_space = gym.spaces.Box(self.env.observation_space.low, self.env.observation_space.high,shape=(self.env.observation_space.shape[0], stepsize), dtype=np.float32)
+        self.action_space = gym.spaces.MultiDiscrete(self.Actionstepsizes)
         print(self.Actionbins)
 
     def action(self, action):
-        for i in range(0, self.action_space.shape[0]):
-            #TODO fix int action
-            action[i] = np.digitize(action[i], self.Actionbins[i])  # return action as index for table
+        #TODO fix actions for more dimensions
+        action = np.digitize(action, self.Actionbins)  # return action as index for table
         return action
