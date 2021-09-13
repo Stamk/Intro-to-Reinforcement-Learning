d961a4277ac85b0871c639ee888d811b63f572ed
Konstantinos <stkostas1995@gmail.com>
2021-09-14 01:53:59 +0300

    some changes in N steps

--- Agents/N_Steps.py
+++ Agents/N_Steps.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 from Agents.q_learning_agent import QAgent
 
 
@@ -22,35 +23,55 @@ class NStepsAgent(QAgent):
         self.rewards = []
 
     def update(self, state, action, new_state, reward, done):
-        self.store_transitions(state, action, reward, done)
-        episode_length = len(self.states)
-        tau = episode_length - self.N + 1
+        if not done: #Initialize and store So, not terminal
+                     #Select and store an Action Ao accordinf to policy
+            self.states.append(state)
+            self.actions.append(action)
+        t=0
+        while True:
+            if t<self.T:
+                 #take action At according to policy
+                 #observe and store the next reward as R(t+1) and the next state as S(t+1)
+                 if t==0:
+                     self.store_transitions(new_state, reward,done)
+                 else:
+                     new_state, reward, done, info = deepcopy(self.env.step(action))
+                     self.store_transitions(new_state, reward,done)
 
-        if tau >= 0:
-            init_state = self.states[tau]
-            init_action = self.actions[tau]
+                 if done:
+                     # if S(t+1) is terminal, then T=t+1
+                     self.T=t+1
+                 else:
+                     #select and store an action A(t+1) according to policy
+                     action = deepcopy(self.choose_action(new_state))
+                     self.actions.append(action)
+            tau=t-self.N+1
             G = 0
-
-            for i in range(tau, min(tau + self.N-1, self.T-1)):
-                G += np.power(self.gamma, i - tau - 1) * self.rewards[i]
-
-            if tau + self.N-1 < self.T: # TODO fix this
+            if tau>=0:
+                for i in range(tau+1, min(tau + self.N, self.T)):
+                    G += np.power(self.gamma, i - tau - 1) * self.rewards[i]
+                if tau + self.N < self.T:
+                    state_tau_plus_n=self.states[tau+self.N]
+                    action_tau_plus_n=self.actions[tau+self.N]
+                    G+=np.power(self.gamma, self.N) * self.q_table[state_tau_plus_n][action_tau_plus_n] # TODO fix this
                 final_state = new_state
                 final_action = self.actions[tau + self.N-1]
                 G += np.power(self.gamma, self.N) * self.value_estimate(final_state, final_action)
+                init_state = self.states[tau]
+                init_action = self.actions[tau]
+                self.q_table[init_state][init_action] += self.lr * (G - self.q_table[init_state][init_action])
+            t=t+1
+            if (tau==self.T-1):break
 
-            self.q_table[init_state][init_action] += self.lr * (
-                    G - self.q_table[init_state][init_action])
-
-    def store_transitions(self, state, action, reward, done):
+    def store_transitions(self, state, reward, done):
         self.states.append(state)
-        self.actions.append(action)
         self.rewards.append(reward)
-
         if done:
             print("End at state {} | number of states {}".format(state, len(self.states)))
             self.T = len(self.states)
             self.init_buffers()
+
+
 
     def value_estimate(self, s, a):
         return NotImplementedError
